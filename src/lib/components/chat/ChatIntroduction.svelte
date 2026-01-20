99e990ecbb36a57836639bdd818029a59426c1d1
root <root@daviddesk2.>
2026-01-20 12:17:57 +0000

    menu improves

--- src/lib/components/chat/ChatIntroduction.svelte
+++ src/lib/components/chat/ChatIntroduction.svelte
@@ -19,18 +19,16 @@
 	});
 </script>
 
-<div class="mt-15 grid items-start justify-left gap-6 px-4 md:mt-32">
-    <div class="flex flex-col select-none items-start text-4xl font-bold md:text-6xl">
+<div class="mt-13 grid items-start justify-left gap-6 px-4 md:mt-32">
+    <div class="flex flex-col select-none items-start text-3xl font-bold md:text-4xl">
    
-<h1 class="font-thin tracking-tight text-gray-900 dark:text-white ">
-        Pretty Smells <br>
-        Chat
-    </h1>
+        <h3 class="font-thin tracking-tight text-gray-900 dark:text-white ">
+            Pretty Smells Chat Demo
+        </h3>
     </div>
 
-    <div class="max-w-2xl space-y-6 text-left my-7">
+    <div class="max-w-2xl space-y-6 text-left my-2">
         <div>
-            <h4 class="font-bold text-lg mb-5">Get Recommendations</h4>
             <p class="text-gray-600 dark:text-gray-300">
                 Our AI identifies unique patterns across your favorite fragrances to build a personalized scent profile.
             </p>
@@ -41,12 +39,6 @@
 
             <p class="text-gray-600 dark:text-gray-300 italic opacity-80 mt-4">
                 Coming soon: Upload a photo of your collection, and we'll instantly analyze your library to find your next signature scent.
-            </p>
-        </div>
-
-        <div class="mt-10">
-            <p class="text-gray-600 dark:text-gray-300 mt-6">
-                <a href="https://www.prettysmellslab.com" class="underline">Return to Store >></a>
             </p>
         </div>
     </div>
